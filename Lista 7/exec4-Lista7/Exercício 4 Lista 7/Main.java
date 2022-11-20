f623978a6e1745ce94936d04fc516b63f108b219
Asaph-Tw <asafe.franca@usp.br>
2022-11-20 11:42:38 -0300

    Falta transformar no arquivo NetB, vou fazer isso no pc do Messias hoje a tarde

---
+++ Lista 7/exec4-Lista7/Exercício 4 Lista 7/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int valorComp = 100000;
+        aleatoriamente teste = new aleatoriamente((valorComp * 10));
+        
+        teste.getListaAL(valorComp);
+        teste.getListaLL(valorComp);
+        teste.atualizaListaAL(valorComp);
+        teste.atualizaListaLL(valorComp);
+    }
+}
