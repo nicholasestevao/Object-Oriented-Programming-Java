f457bef061668469f15704046847bb220d029d70
nicholasestevao <nicholas.estevao@usp.br>
2022-12-20 11:27:39 -0300

    ex 1 lista 11 parcial

--- Lista 11/ex1-Lista11/ex1-Lista11-Cliente/src/ex1/lista11/cliente/Ex1Lista11Cliente.java
+++ Lista 11/ex1-Lista11/ex1-Lista11-Cliente/src/ex1/lista11/cliente/Ex1Lista11Cliente.java
@@ -4,7 +4,6 @@ package ex1.lista11.cliente;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,41 +11,48 @@ import java.net.Socket;
 public class Ex1Lista11Cliente {
     public static void main(String[] args) {
         try {
-            Socket CLIENTE_SOCKET = new Socket("localhost", 8008);     
-            OutputStreamWriter saida = new OutputStreamWriter(CLIENTE_SOCKET.getOutputStream());
-        
-            PrintWriter ENVIA = new PrintWriter(new OutputStreamWriter(CLIENTE_SOCKET.getOutputStream()));
+            //ServerSocket OUVIDO = new ServerSocket(8000);
+            //Socket CLIENTE_SOCKET = OUVIDO.accept();
+            Socket CLIENTE_SOCKET = new Socket("localhost", 8008);
+            Thread recebe = new Thread(new ThreadRecebe(CLIENTE_SOCKET));
+            recebe.start();
+            Thread envia = new Thread(new ThreadEnvia(CLIENTE_SOCKET));
+            envia.start();
             
+            //Thread threadServidor = new Thread(new ThreadServidor(SERVIDOR_SOCKET));
+            //threadServidor.start();
+            
+            recebe.join();
+            envia.join();
+            CLIENTE_SOCKET.close();
+            
+            
+            /*ServerSocket OUVIDO = new ServerSocket(8000);
+            Socket SERVIDOR_SOCKET = OUVIDO.accept();
+            
+            
+            
+            Thread recebe = new Thread(new ThreadRecebe(CLIENTE_SOCKET));
+            Thread envia = new Thread(new ThreadEnvia(SERVIDOR_SOCKET));
+            
+            envia.start();
+            recebe.start();*/
+            //CLIENTE_SOCKET.close();
+            /*PrintWriter ENVIA = new PrintWriter(CLIENTE_SOCKET.getOutputStream(), true);
             BufferedReader RECEBE = new BufferedReader(new InputStreamReader(CLIENTE_SOCKET.getInputStream()));
-            //Mensagem inicial para autenticar conexao
 
             BufferedReader LEITOR_ENTRADA_PADRAO = new BufferedReader(new InputStreamReader(System.in));
-            String msg_inicial = "";
-            msg_inicial = LEITOR_ENTRADA_PADRAO.readLine();
-            ENVIA.println(msg_inicial); 
-            ENVIA.flush();
-            String res_autenticacao = RECEBE.readLine();
-            
-            if(res_autenticacao.equals("true")){
-                System.out.println("Conexao autenticada");
-                Thread envia = new Thread(new ThreadEnvia(ENVIA));
-                envia.start();
-                envia.join();
-                System.out.println("Cliente terminou de enviar mensagens");
-
-                Thread recebe = new Thread(new ThreadRecebe(RECEBE));
-                recebe.start();
-                recebe.join();
-                System.out.println("Cliente terminou de receber mensagens");
-            }else{
-                System.out.println("Conexao nao autenticada");
+            String userInput = "";
+            while(userInput.compareTo("BYE") != 0){
+                userInput = LEITOR_ENTRADA_PADRAO.readLine();
+                ENVIA.println("O cliente diz \"" + userInput +"\"");                
+                System.out.println(RECEBE.readLine());                         
             }
-            
-            LEITOR_ENTRADA_PADRAO.close();
+
             ENVIA.close();
             RECEBE.close();
-            CLIENTE_SOCKET.close();
-
+            LEITOR_ENTRADA_PADRAO.close();
+            CLIENTE_SOCKET.close();*/
         } catch (IOException | InterruptedException e) {
             System.out.println("Erro: " + e.getMessage());
         }
